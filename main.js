c9d34feed3e6f01c405a7fdd31ff0ec204628e9c
shaharni <91742717+shaharni@users.noreply.github.com>
2022-08-24 18:20:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Adi Narayana Chollangi", "Ganesh War Rao Chollangi", "Tanish Chollangi", "Shaharni Chollangi", "Lalitha Swathi Chollangi"];
+var names = ["Family Book","Adi Narayana Chollangi", "Ganesh War Rao Chollangi", "Tanish Chollangi", "Shaharni Chollangi", "Lalitha Swathi Chollangi"];
 var i = 0;
 function update()
 {
     i++;
     var numbers_of_family_member_in_array = 5
     if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
+    {
+      i = 0;
+    }
     
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [i];
+    var updatedImage = images[i];
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [i] ;
+    var updatedName = names[i];
  
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
